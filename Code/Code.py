acbf01931d4ba7037d0c183474b6e982dd48ea80
MrDevenC <66448892+MrDevenC@users.noreply.github.com>
2021-06-21 16:30:38 +1200

    PSR.py

--- Code/Code.py
+++ Code/Code.py
@@ -1,2 +1,9 @@
 import random
-psr = input("
+counter = 0
+psr = input("Would you like to play Paper, Scissors, Rock? The choices will be r, p and s which mean rock, paper,scissors. Type: ready when you are ready.")
+if psr.lower() == "ready" :
+    print("ok, now we will start playing Paper, Scissors, Rock")
+user = input("Enter a choice (r, p, s): ")
+possible_actions = ["r","p","s"]
+computer = random.choice(possible_actions)
+print(f"\nYou chose {user}, computer chose {computer}.\n")
